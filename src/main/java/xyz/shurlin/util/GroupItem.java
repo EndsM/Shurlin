29c4475bca825bdc9c518eb317d4bf0b584c44a8
EndsM <z2zz2zz@yahoo.com>
2024-04-09 19:33:02 +0800

    Update GroupItem.java
    Remove excessive ;

--- src/main/java/xyz/shurlin/util/GroupItem.java
+++ src/main/java/xyz/shurlin/util/GroupItem.java
@@ -7,18 +7,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public enum GroupItem {
-    BREAKABLE(Items.ACACIA_LEAVES,Items.BIRCH_LEAVES,Items.DARK_OAK_LEAVES,Items.JUNGLE_LEAVES,Items.OAK_LEAVES, Items.SPRUCE_LEAVES
+    BREAKABLE(Items.ACACIA_LEAVES, Items.BIRCH_LEAVES, Items.DARK_OAK_LEAVES, Items.JUNGLE_LEAVES, Items.OAK_LEAVES, Items.SPRUCE_LEAVES
 
     );
 
     private final ArrayList<Item> itemList = new ArrayList<>();
 //    private ArrayList<Tag> tagList = new ArrayList<>();
 
-    GroupItem(Item... items){
+    GroupItem(Item... items) {
         itemList.addAll(Arrays.asList(items));
-    };
+    }
 
-    public static boolean canInsert(GroupItem groupItem, Item item, ShurlinLevel level){
+
+
+    public static boolean canInsert(GroupItem groupItem, Item item, ShurlinLevel level) {
         return item instanceof ShurlinLevel ?
                 (groupItem.itemList.contains(item) && Utils.canDo(level, (ShurlinLevel) item)) :
                 groupItem.itemList.contains(item);
