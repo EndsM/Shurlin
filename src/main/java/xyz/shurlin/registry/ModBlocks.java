7f8ee205ce30e25332d9470c37041772e339093e
EndsM <z2zz2zz@yahoo.com>
2024-04-12 03:27:56 +0800

    implement the Register for block

--- src/main/java/xyz/shurlin/registry/ModBlocks.java
+++ src/main/java/xyz/shurlin/registry/ModBlocks.java
@@ -1,7 +1,10 @@
 package xyz.shurlin.registry;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xyz.shurlin.Shurlin;
@@ -9,11 +12,13 @@ import xyz.shurlin.Shurlin;
 public class ModBlocks {
     private static final Logger logger = LogManager.getLogger(Shurlin.MODID);
 
-    private Block registerBlock() {
-
+    private Block registerBlock(String name, Block block) {
+        Block registeredBlock = Registry.register(Registry.BLOCK, new Identifier(Shurlin.MODID, name), block);
+        return registeredBlock;
     }
 
-    private Item registerBlockItem() {
-
+    private Item registerBlockItem(String name, Block block) {
+        Item registeredBlockItem = Registry.register(Registry.ITEM, new Identifier(Shurlin.MODID, name), new BlockItem(block, new Item.Settings()));
+        return registeredBlockItem;
     }
 }
--- src/main/java/xyz/shurlin/registry/ModItems.java
+++ src/main/java/xyz/shurlin/registry/ModItems.java
@@ -1,4 +1,10 @@
 package xyz.shurlin.registry;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import xyz.shurlin.Shurlin;
+
 public class ModItems {
+    private static final Logger logger = LogManager.getLogger(Shurlin.MODID);
 }
