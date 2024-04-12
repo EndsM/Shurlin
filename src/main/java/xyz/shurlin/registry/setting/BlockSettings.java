91f0c8d19c063d0e9b971ce61110cbd97010545c
EndsM <z2zz2zz@yahoo.com>
2024-04-12 20:20:52 +0800

    Make settings more readable

--- src/main/java/xyz/shurlin/registry/setting/BlockSettings.java
+++ src/main/java/xyz/shurlin/registry/setting/BlockSettings.java
@@ -24,32 +24,54 @@ public class BlockSettings {
 
     static {
         PLANT = FabricBlockSettings.of(Material.PLANT)
-                .noCollision().ticksRandomly().breakInstantly().nonOpaque().sounds(BlockSoundGroup.GRASS);
+                .noCollision()
+                .ticksRandomly()
+                .breakInstantly()
+                .nonOpaque()
+                .sounds(BlockSoundGroup.GRASS);
         LOG = FabricBlockSettings.of(Material.WOOD)
-                .strength(2.0f).sounds(BlockSoundGroup.WOOD);
+                .strength(2.0f)
+                .sounds(BlockSoundGroup.WOOD);
         LEAVES = FabricBlockSettings.of(Material.LEAVES)
-                .strength(0.2f, 0.2f).ticksRandomly().sounds(BlockSoundGroup.GRASS).nonOpaque();
+                .strength(0.2f, 0.2f)
+                .ticksRandomly()
+                .sounds(BlockSoundGroup.GRASS)
+                .nonOpaque();
         PLANKS = FabricBlockSettings.of(Material.WOOD)
-                .strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD);
+                .strength(2.0F, 3.0F)
+                .sounds(BlockSoundGroup.WOOD);
         ORE = FabricBlockSettings.of(Material.STONE)
-                .requiresTool().strength(3.0f, 3.0f);
+                .requiresTool()
+                .strength(3.0f, 3.0f);
         CORAL = FabricBlockSettings.of(Material.UNDERWATER_PLANT, MapColor.GREEN)
-                .noCollision().breakInstantly().sounds(BlockSoundGroup.WET_GRASS);
+                .noCollision()
+                .breakInstantly()
+                .sounds(BlockSoundGroup.WET_GRASS);
         DEAD_CORAL = FabricBlockSettings.of(Material.STONE, MapColor.GRAY)
-                .requiresTool().noCollision().breakInstantly();
+                .requiresTool()
+                .noCollision()
+                .breakInstantly();
         CORAL_BLOCK = FabricBlockSettings.of(Material.STONE, MapColor.GREEN)
-                .requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.CORAL);
+                .requiresTool()
+                .strength(1.5F, 6.0F)
+                .sounds(BlockSoundGroup.CORAL);
         DEAD_CORAL_BLOCK = FabricBlockSettings.of(Material.STONE, MapColor.GRAY)
-                .requiresTool().strength(1.5F, 6.0F);
+                .requiresTool()
+                .strength(1.5F, 6.0F);
         OBSIDIAN = FabricBlockSettings.of(Material.STONE)
-                .requiresTool().strength(60.0F, 1200.0F);
+                .requiresTool()
+                .strength(60.0F, 1200.0F);
         WORKER = FabricBlockSettings.of(Material.STONE)
                 .strength(10.0f, 10.0f);
         STONE = FabricBlockSettings.of(Material.STONE, MapColor.STONE_GRAY)
-                .requiresTool().strength(1.5F, 6.0F);
+                .requiresTool()
+                .strength(1.5F, 6.0F);
         DIRT = FabricBlockSettings.of(Material.SOIL, MapColor.DIRT_BROWN)
-                .strength(0.5F).sounds(BlockSoundGroup.GRAVEL);
+                .strength(0.5F)
+                .sounds(BlockSoundGroup.GRAVEL);
         LLANDUDNO = FabricBlockSettings.of(Material.METAL)
-                .strength(20.0f, 100.0f).requiresTool().sounds(BlockSoundGroup.METAL);
+                .strength(20.0f, 100.0f)
+                .requiresTool()
+                .sounds(BlockSoundGroup.METAL);
     }
 }
