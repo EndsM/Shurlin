e52425c938c484f64d1c2492c20d68eed8755538
EndsM <z2zz2zz@yahoo.com>
2024-04-14 03:14:39 +0800

    Comment the plan
    Oh boy, here we go again

--- src/main/java/xyz/shurlin/registry/ModConfiguredFeatures.java
+++ src/main/java/xyz/shurlin/registry/ModConfiguredFeatures.java
@@ -2,4 +2,6 @@ package xyz.shurlin.registry;
 
 public class ModConfiguredFeatures {
     // Experimental solution to register problem
+// Use this to combine ShurlinOreFeatures and ShurlinConfiguredFeatures
+    // And separate them with comments
 }
