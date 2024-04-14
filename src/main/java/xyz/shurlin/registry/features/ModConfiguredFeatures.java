b77c99b955a8d654e719ee3ae5acb68f6bb7f194
EndsM <z2zz2zz@yahoo.com>
2024-04-14 17:24:27 +0800

    First tree copied

--- src/main/java/xyz/shurlin/registry/features/ModConfiguredFeatures.java
+++ src/main/java/xyz/shurlin/registry/features/ModConfiguredFeatures.java
@@ -1,18 +1,38 @@
 package xyz.shurlin.registry.features;
 
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.world.gen.UniformIntDistribution;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
-import net.minecraft.world.gen.stateprovider.BlockStateProvider;
+import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
+import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
+import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.stateprovider.WeightedBlockStateProvider;
+import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
+import xyz.shurlin.registry.ModBlocks;
 
 public class ModConfiguredFeatures {
     // Experimental solution to register problem
     // Use this to combine ShurlinOreFeatures and ShurlinConfiguredFeatures
     // And separate them with comments
-    public static ConfiguredFeature<TreeFeatureConfig, ?> PEAR_TREE;
+    public static ConfiguredFeature<TreeFeatureConfig, ?> PEAR_TREE = Feature.TREE.configure(new TreeFeatureConfig.Builder(
+            new SimpleBlockStateProvider(ModBlocks.PEAR_LOG.getDefaultState()),
+            // This config the weight of leaves with pear and without pear
+            new WeightedBlockStateProvider()
+                    .addState(ModBlocks.PEAR_LEAVES.getDefaultState().with(LeavesBlock.PERSISTENT, true), 9)
+                    .addState(ModBlocks.PEAR_RIPE_LEAVES.getDefaultState().with(LeavesBlock.PERSISTENT, true), 1),
+            new BlobFoliagePlacer(UniformIntDistribution.of(2), UniformIntDistribution.of(0), 3),
+            new StraightTrunkPlacer(5, 2, 0),
+            new TwoLayersFeatureSize(1, 0, 1)
+    ).build());
 
+    private static void registerConfiguredFeature(){
+
+    }
 
     // Accessible in the same package
-    static void Register(){
+    static void Register() {
 
     }
 }
