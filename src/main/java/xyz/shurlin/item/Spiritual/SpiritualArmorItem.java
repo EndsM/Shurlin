184ce53abfd57e5a874595509ee5d0f154049ee7
EndsM <z2zz2zz@yahoo.com>
2024-04-23 14:51:46 +0800

    give SpiritualArmorItem SpiritualLevel

--- src/main/java/xyz/shurlin/item/Spiritual/SpiritualArmorItem.java
+++ src/main/java/xyz/shurlin/item/Spiritual/SpiritualArmorItem.java
@@ -4,9 +4,18 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import xyz.shurlin.item.ItemGroups;
+import xyz.shurlin.util.ShurlinLevel;
 
-public class SpiritualArmorItem extends ArmorItem {
-    public SpiritualArmorItem(ArmorMaterial material, EquipmentSlot slot) {
+public class SpiritualArmorItem extends ArmorItem implements ShurlinLevel {
+    private final float shurlinLevel;
+
+    public SpiritualArmorItem(ArmorMaterial material, EquipmentSlot slot, float shurlinLevel) {
         super(material, slot, new Settings().group(ItemGroups.SHURLIN));
+        this.shurlinLevel = shurlinLevel;
+    }
+
+    @Override
+    public float getShurlinLevel() {
+        return shurlinLevel;
     }
 }
--- src/main/java/xyz/shurlin/registry/helper/ModArmorItems.java
+++ src/main/java/xyz/shurlin/registry/helper/ModArmorItems.java
@@ -1,13 +1,16 @@
 package xyz.shurlin.registry.helper;
 
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.shurlin.Shurlin;
+import xyz.shurlin.item.ArmorMaterials;
+import xyz.shurlin.item.Spiritual.SpiritualArmorItem;
 
 public class ModArmorItems {
     // Plant Iron
-
+    public static Item PLANT_IRON_HELMET = new SpiritualArmorItem(ArmorMaterials.PLANT_IRON, EquipmentSlot.HEAD, 1.0f);
     // Plant Gold
 
     // Plant Jade
