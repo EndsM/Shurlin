92f1e380613f6af8407b11e588ed9abcb08ac0eb
EndsM <z2zz2zz@yahoo.com>
2024-04-23 14:47:21 +0800

    scaffolding the class

--- src/main/java/xyz/shurlin/registry/helper/ModArmorItems.java
+++ src/main/java/xyz/shurlin/registry/helper/ModArmorItems.java
@@ -1,7 +1,19 @@
 package xyz.shurlin.registry.helper;
 
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import xyz.shurlin.Shurlin;
+
 public class ModArmorItems {
-    private static void register() {
+    // Plant Iron
+
+    // Plant Gold
+
+    // Plant Jade
+
+    private static void register(String name, Item item) {
+        Registry.register(Registry.ITEM, new Identifier(Shurlin.MODID, name), item);
     }
 
     public static void Register() {
