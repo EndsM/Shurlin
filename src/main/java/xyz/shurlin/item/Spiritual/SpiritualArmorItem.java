2f046b20bc0c326ebc8fde8f3466a74fa9ae4ed6
EndsM <z2zz2zz@yahoo.com>
2024-04-23 14:37:52 +0800

    Update SpiritualArmorItem.java

--- src/main/java/xyz/shurlin/item/Spiritual/SpiritualArmorItem.java
+++ src/main/java/xyz/shurlin/item/Spiritual/SpiritualArmorItem.java
@@ -1,4 +1,12 @@
 package xyz.shurlin.item.Spiritual;
 
-public class SpiritualArmorItem {
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import xyz.shurlin.item.ItemGroups;
+
+public class SpiritualArmorItem extends ArmorItem {
+    public SpiritualArmorItem(ArmorMaterial material, EquipmentSlot slot) {
+        super(material, slot, new Settings().group(ItemGroups.SHURLIN));
+    }
 }
