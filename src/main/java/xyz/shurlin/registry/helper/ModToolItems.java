367dff4c1d8036503cb70ca997a86bfa7d2425fd
EndsM <z2zz2zz@yahoo.com>
2024-04-23 13:38:02 +0800

    Start actually registry

--- src/main/java/xyz/shurlin/registry/helper/ModToolItems.java
+++ src/main/java/xyz/shurlin/registry/helper/ModToolItems.java
@@ -4,12 +4,14 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.shurlin.Shurlin;
+import xyz.shurlin.item.Spiritual.SpiritualPickaxeItem;
 import xyz.shurlin.item.Spiritual.SpiritualSwordItem;
 import xyz.shurlin.item.ToolMaterials;
 
 public class ModToolItems {
     // Plant Iron
     public static final Item PLANT_IRON_SWORD = new SpiritualSwordItem(ToolMaterials.PLANT_IRON, 1.0f);
+    public static final Item PLAN_IRON_PICKAXE = new SpiritualPickaxeItem(ToolMaterials.PLANT_IRON, 1.0f);
     // Plant Gold
 
     // Plant Jade
@@ -19,5 +21,7 @@ public class ModToolItems {
     }
 
     public static void Register() {
+        register("plant_iron_sword", PLANT_IRON_SWORD);
+        register("plant_iron_pickaxe", PLAN_IRON_PICKAXE);
     }
 }
