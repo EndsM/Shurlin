11c47b3bc67609a55b752a6c9dfe71a487338226
EndsM <z2zz2zz@yahoo.com>
2024-04-23 13:41:22 +0800

    Update ModToolItems.java
    a typo is fixed

--- src/main/java/xyz/shurlin/registry/helper/ModToolItems.java
+++ src/main/java/xyz/shurlin/registry/helper/ModToolItems.java
@@ -4,14 +4,16 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.shurlin.Shurlin;
-import xyz.shurlin.item.Spiritual.SpiritualPickaxeItem;
-import xyz.shurlin.item.Spiritual.SpiritualSwordItem;
+import xyz.shurlin.item.Spiritual.*;
 import xyz.shurlin.item.ToolMaterials;
 
 public class ModToolItems {
     // Plant Iron
     public static final Item PLANT_IRON_SWORD = new SpiritualSwordItem(ToolMaterials.PLANT_IRON, 1.0f);
-    public static final Item PLAN_IRON_PICKAXE = new SpiritualPickaxeItem(ToolMaterials.PLANT_IRON, 1.0f);
+    public static final Item PLANT_IRON_PICKAXE = new SpiritualPickaxeItem(ToolMaterials.PLANT_IRON, 1.0f);
+    public static final Item PLANT_IRON_AXE = new SpiritualAxeItem(ToolMaterials.PLANT_IRON, 1.0f);
+    public static final Item PLANT_IRON_SHOVEL = new SpiritualShovelItem(ToolMaterials.PLANT_IRON, 1.0f);
+    public static final Item PLANT_IRON_HOE = new SpiritualHoeItem(ToolMaterials.PLANT_IRON, 1.0f);
     // Plant Gold
 
     // Plant Jade
@@ -22,6 +24,6 @@ public class ModToolItems {
 
     public static void Register() {
         register("plant_iron_sword", PLANT_IRON_SWORD);
-        register("plant_iron_pickaxe", PLAN_IRON_PICKAXE);
+        register("plant_iron_pickaxe", PLANT_IRON_PICKAXE);
     }
 }
