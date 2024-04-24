07c197b28a9fd2517a3eebb0ea16c8d6be4605f0
EndsM <z2zz2zz@yahoo.com>
2024-04-24 15:46:19 +0800

    original config is very hard to extract
    So will take a few commits to do it

--- src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
+++ src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
@@ -1,7 +1,24 @@
 package xyz.shurlin.registry.features;
 
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import xyz.shurlin.Shurlin;
+import xyz.shurlin.registry.ModBlocks;
 
 public class ModOreFeatures {
-    public static ConfiguredFeature<?, ?> ORE_PLANT_IRON;
+    public static ConfiguredFeature<?, ?> ORE_PLANT_IRON = Feature.ORE.configure(new OreFeatureConfig(
+            OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, ModBlocks.PLANT_IRON_ORE_BLOCK.getDefaultState(), 6
+    ));
+
+    private static void registerOreFeature(String id, ConfiguredFeature<?, ?> configuredFeature) {
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Shurlin.MODID, id), configuredFeature);
+    }
+
+    public static void Register() {
+
+    }
 }
