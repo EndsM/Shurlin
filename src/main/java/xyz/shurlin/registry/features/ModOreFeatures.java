6b816c8475558d1ccef3b9a761abd183d3b89d4c
EndsM <z2zz2zz@yahoo.com>
2024-04-24 15:29:43 +0800

    prepare to move OreFeatures registry

--- src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
+++ src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
@@ -1,4 +1,7 @@
 package xyz.shurlin.registry.features;
 
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+
 public class ModOreFeatures {
+    public static ConfiguredFeature<?, ?> ORE_PLANT_IRON;
 }
