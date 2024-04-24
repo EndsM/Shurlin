5c2ae5a215d0e5cd1b2ea652423095b87b9a2ac1
EndsM <z2zz2zz@yahoo.com>
2024-04-24 15:48:52 +0800

    So this is the config itself

--- src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
+++ src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
@@ -3,6 +3,8 @@ package xyz.shurlin.registry.features;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
@@ -12,7 +14,7 @@ import xyz.shurlin.registry.ModBlocks;
 public class ModOreFeatures {
     public static ConfiguredFeature<?, ?> ORE_PLANT_IRON = Feature.ORE.configure(new OreFeatureConfig(
             OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, ModBlocks.PLANT_IRON_ORE_BLOCK.getDefaultState(), 6
-    ));
+    )).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0,0,48))).repeat(6);
 
     private static void registerOreFeature(String id, ConfiguredFeature<?, ?> configuredFeature) {
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Shurlin.MODID, id), configuredFeature);
