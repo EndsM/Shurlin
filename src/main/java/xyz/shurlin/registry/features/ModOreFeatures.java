b071c039bf98db16a39c0b1decf1fe6adfdf2d80
EndsM <z2zz2zz@yahoo.com>
2024-04-24 17:25:48 +0800

    The apply it in Features registry class

--- src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
+++ src/main/java/xyz/shurlin/registry/features/ModOreFeatures.java
@@ -12,15 +12,20 @@ import xyz.shurlin.Shurlin;
 import xyz.shurlin.registry.ModBlocks;
 
 public class ModOreFeatures {
-    public static ConfiguredFeature<?, ?> ORE_PLANT_IRON = Feature.ORE.configure(new OreFeatureConfig(
-            OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, ModBlocks.PLANT_IRON_ORE_BLOCK.getDefaultState(), 6
-    )).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0,0,48))).repeat(6);
+    public static ConfiguredFeature<?, ?> ORE_PLANT_IRON = Feature.ORE
+            .configure(new OreFeatureConfig(
+                    OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+                    ModBlocks.PLANT_IRON_ORE_BLOCK.getDefaultState(),
+                    6
+            ))
+            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 48)))
+            .repeat(6);
 
     private static void registerOreFeature(String id, ConfiguredFeature<?, ?> configuredFeature) {
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Shurlin.MODID, id), configuredFeature);
     }
 
     public static void Register() {
-
+        registerOreFeature("ore_plant_iron", ORE_PLANT_IRON);
     }
 }
