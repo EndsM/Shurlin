e17c0e9832c888b037c1e9fb28e1fcc732b9fc78
EndsM <z2zz2zz@yahoo.com>
2024-04-27 16:49:20 +0800

    I found that I may need a data fixer

--- src/main/java/xyz/shurlin/registry/ModBlockEntities.java
+++ src/main/java/xyz/shurlin/registry/ModBlockEntities.java
@@ -2,10 +2,11 @@ package xyz.shurlin.registry;
 
 
 import net.minecraft.block.entity.BlockEntityType;
+import xyz.shurlin.block.entity.CultivationCrystalBlockEntity;
 
 public class ModBlockEntities {
     // I guess I also need to move this, so I can migrate the language feature of original one
-    public static BlockEntityType CULTIVATION_CRYSTAL_BLOCK_ENTITY;
+    public static BlockEntityType<CultivationCrystalBlockEntity> CULTIVATION_CRYSTAL_BLOCK_ENTITY = BlockEntityType.Builder.create(CultivationCrystalBlockEntity::new, ModBlocks.CULTIVATION_CRYSTAL).build(null);
 
     private static void registerBlockEntity() {
 
