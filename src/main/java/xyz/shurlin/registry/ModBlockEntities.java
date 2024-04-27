7af2bfa3bd8194291047d76d9cf7dfe9228008a3
EndsM <z2zz2zz@yahoo.com>
2024-04-27 16:33:35 +0800

    Create ModBlockEntities.java
    Will impl it later

---
+++ src/main/java/xyz/shurlin/registry/ModBlockEntities.java
@@ -0,0 +1,9 @@
+package xyz.shurlin.registry;
+
+
+import net.minecraft.block.entity.BlockEntityType;
+
+public class ModBlockEntities {
+    // I guess I also need to move this, so I can migrate the language feature of original one
+    public static BlockEntityType CULTIVATION_CRYSTAL_BLOCK_ENTITY;
+}
