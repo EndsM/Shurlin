e113233baca828fecf017fa5b0792b76bf956f94
EndsM <z2zz2zz@yahoo.com>
2024-04-27 17:05:52 +0800

    So add it to TODO list

--- src/main/java/xyz/shurlin/registry/ModBlockEntities.java
+++ src/main/java/xyz/shurlin/registry/ModBlockEntities.java
@@ -2,17 +2,20 @@ package xyz.shurlin.registry;
 
 
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import xyz.shurlin.Shurlin;
 import xyz.shurlin.block.entity.CultivationCrystalBlockEntity;
 
 public class ModBlockEntities {
-    // I guess I also need to move this, so I can migrate the language feature of original one
+    // TODO: implement datafixer in higher version of Minecraft, use .build(null) as temp solution
     public static BlockEntityType<CultivationCrystalBlockEntity> CULTIVATION_CRYSTAL_BLOCK_ENTITY = BlockEntityType.Builder.create(CultivationCrystalBlockEntity::new, ModBlocks.CULTIVATION_CRYSTAL).build(null);
 
-    private static void registerBlockEntity() {
-
+    private static void registerBlockEntity(String id, BlockEntityType<?> blockEntityType) {
+        Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(Shurlin.MODID, id), blockEntityType);
     }
 
     public static void Register() {
-
+        registerBlockEntity("cultivation_crystal_block_entity", CULTIVATION_CRYSTAL_BLOCK_ENTITY);
     }
 }
