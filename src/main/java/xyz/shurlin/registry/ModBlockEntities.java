d5daccd16efd26b5346b3f7b3ec46e42ba17d111
EndsM <z2zz2zz@yahoo.com>
2024-04-27 16:35:55 +0800

    Add some todo functions

--- src/main/java/xyz/shurlin/registry/ModBlockEntities.java
+++ src/main/java/xyz/shurlin/registry/ModBlockEntities.java
@@ -6,4 +6,12 @@ import net.minecraft.block.entity.BlockEntityType;
 public class ModBlockEntities {
     // I guess I also need to move this, so I can migrate the language feature of original one
     public static BlockEntityType CULTIVATION_CRYSTAL_BLOCK_ENTITY;
+
+    private static void registerBlockEntity() {
+
+    }
+
+    public static void Register() {
+
+    }
 }
