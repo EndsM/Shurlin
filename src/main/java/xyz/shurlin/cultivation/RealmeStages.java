8439f26bf2075c08dd8e1f67eb309c4dc3914a74
EndsM <z2zz2zz@yahoo.com>
2024-04-29 23:11:24 +0800

    SimpleLevels -> RealmeStages

--- src/main/java/xyz/shurlin/cultivation/RealmeStages.java
+++ src/main/java/xyz/shurlin/cultivation/RealmeStages.java
@@ -1,6 +1,6 @@
 package xyz.shurlin.cultivation;
 
-public enum SimpleLevels {
+public enum RealmeStages {
     LOW(1, "", "low"),
     MIDDLE(2, "§a", "middle"),
     HIGH(3, "§9", "high");
@@ -9,7 +9,7 @@ public enum SimpleLevels {
     private final String color;
     private final String name;
 
-    SimpleLevels(int level, String color, String name) {
+    RealmeStages(int level, String color, String name) {
         this.level = level;
         this.color = color;
         this.name = name;
