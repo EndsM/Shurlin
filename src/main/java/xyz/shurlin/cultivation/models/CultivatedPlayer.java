c7aa58512de1c21b78b66bace58143bc5c1fcfa6
EndsM <z2zz2zz@yahoo.com>
2024-05-02 23:28:48 +0800

    Add more explain on this topic
    in the form of comments

--- src/main/java/xyz/shurlin/cultivation/models/CultivatedPlayer.java
+++ src/main/java/xyz/shurlin/cultivation/models/CultivatedPlayer.java
@@ -1,6 +1,8 @@
 package xyz.shurlin.cultivation.models;
 
+import net.minecraft.client.realms.Realms;
 import xyz.shurlin.cultivation.models.enums.CultivationType;
+import xyz.shurlin.cultivation.models.enums.RealmStage;
 
 import java.util.List;
 
@@ -9,8 +11,14 @@ public class CultivatedPlayer {
     private CultivationType cultivationType;
     // Realms of a cultivation system will not be hard coded, though I need put more thought to it later
     // It will be created for player as they select their cultivation system, and write to their dataset.
-    // However, this solution need be ware of the system's update, if player already have the dataset in them
+    // However, this solution need beware of the system's update, if player already have the dataset in them
     private List<CultivationRealm> cultivationStages;
     // Use this as temp solution of specify which stage player is on
+    // Range[0, cultivationType.numberOfRealms - 1]
     private int currentStage;
+    // This will represent the progress in current realm
+    // Once reach cultivationStages[currentStage].progressBar, will set as this value,
+    // then wait for breakthrough
+    private double currentCulProgress;
+    private RealmStage realmStage;
 }
--- src/main/java/xyz/shurlin/cultivation/models/CultivationRealm.java
+++ src/main/java/xyz/shurlin/cultivation/models/CultivationRealm.java
@@ -8,4 +8,6 @@ public class CultivationRealm {
     // Sometimes the name of the energy from the cultivation system would change according to realm
     // example: 真气->真元
     private String energyNameKey;
+    // cultivation exp needed in this realm
+    private double progressBar;
 }
