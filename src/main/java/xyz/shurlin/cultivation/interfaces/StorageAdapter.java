5bc3509c9559b506c129d980b6d9adbbbfa96b1a
EndsM <z2zz2zz@yahoo.com>
2024-05-04 21:52:40 +0800

    Add some parameters to StorageAdapter

--- src/main/java/xyz/shurlin/cultivation/interfaces/StorageAdapter.java
+++ src/main/java/xyz/shurlin/cultivation/interfaces/StorageAdapter.java
@@ -4,14 +4,15 @@ import xyz.shurlin.cultivation.models.CultivationRealm;
 import xyz.shurlin.cultivation.models.enums.CultivationType;
 
 import java.util.List;
+import java.util.Map;
 
 
 public interface StorageAdapter {
-    boolean SaveCultivationType();
+    boolean SaveCultivationType(CultivationType cultivationType);
 
     CultivationType LoadCultivationType();
 
-    boolean SaveCultivationStages();
+    boolean SaveCultivationStages(Map<Integer, CultivationRealm> cultivationStages);
 
     List<CultivationRealm> LoadCultivationStages();
 }
--- src/main/java/xyz/shurlin/cultivation/mixin/MixinStorageAdapter.java
+++ src/main/java/xyz/shurlin/cultivation/mixin/MixinStorageAdapter.java
@@ -3,19 +3,23 @@ package xyz.shurlin.cultivation.mixin;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.shurlin.cultivation.interfaces.StorageAdapter;
+import xyz.shurlin.cultivation.models.CultivatedPlayer;
 import xyz.shurlin.cultivation.models.CultivationRealm;
 import xyz.shurlin.cultivation.models.enums.CultivationType;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Mixin(PlayerEntity.class)
 public abstract class MixinStorageAdapter implements StorageAdapter {
-
+    @Unique
+    private CultivatedPlayer cultivatedPlayer;
 
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
     private void writeToNbt(NbtCompound nbt, CallbackInfo ci) {
@@ -26,7 +30,7 @@ public abstract class MixinStorageAdapter implements StorageAdapter {
     }
 
     @Override
-    public boolean SaveCultivationType() {
+    public boolean SaveCultivationType(CultivationType cultivationType) {
         // To be implemented
         return false;
     }
@@ -37,7 +41,7 @@ public abstract class MixinStorageAdapter implements StorageAdapter {
     }
 
     @Override
-    public boolean SaveCultivationStages() {
+    public boolean SaveCultivationStages(Map<Integer, CultivationRealm> cultivationStages) {
         return false;
     }
 
