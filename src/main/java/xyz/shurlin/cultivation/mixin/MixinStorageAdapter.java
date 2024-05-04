0972deac2acb285493894121686a2f946205a338
EndsM <z2zz2zz@yahoo.com>
2024-05-05 00:07:06 +0800

    Update MixinStorageAdapter.java
    Create an example mixin and a test obj

--- src/main/java/xyz/shurlin/cultivation/mixin/MixinStorageAdapter.java
+++ src/main/java/xyz/shurlin/cultivation/mixin/MixinStorageAdapter.java
@@ -11,6 +11,7 @@ import xyz.shurlin.cultivation.interfaces.StorageAdapter;
 import xyz.shurlin.cultivation.models.CultivatedPlayer;
 import xyz.shurlin.cultivation.models.CultivationRealm;
 import xyz.shurlin.cultivation.models.enums.CultivationType;
+import xyz.shurlin.cultivation.models.enums.RealmStage;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,11 +19,38 @@ import java.util.Map;
 
 @Mixin(PlayerEntity.class)
 public abstract class MixinStorageAdapter implements StorageAdapter {
+    private static final String namespace = "cultivation";
+
     @Unique
-    private CultivatedPlayer cultivatedPlayer;
+    private CultivatedPlayer cultivatedPlayer ;
+    MixinStorageAdapter(){
+        this.cultivatedPlayer = new CultivatedPlayer();
+        this.cultivatedPlayer.SetCultivationType(CultivationType.SHURLIN); // Initialize with a default cultivation type
+        this.cultivatedPlayer.SetCultivationStages(Collections.singletonMap(1, new CultivationRealm())); // Initialize with a default cultivation stage
+        this.cultivatedPlayer.SetCurrentStage(0); // Initialize with a default current stage
+        this.cultivatedPlayer.SetCurrentCulProgress(0.0); // Initialize with a default current cultivation progress
+        this.cultivatedPlayer.SetRealmStage(RealmStage.LOW); // Initialize with a default realm stage
+
+    }
 
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
     private void writeToNbt(NbtCompound nbt, CallbackInfo ci) {
+        if (cultivatedPlayer != null) {
+            NbtCompound CultivationData = new NbtCompound();
+            CultivationData.putString("CultivationType", cultivatedPlayer.GetCultivationType().name());
+            CultivationData.putInt("CurrentStage", cultivatedPlayer.GetCurrentStage());
+            CultivationData.putDouble("CurrentCulProgress", cultivatedPlayer.GetCurrentCulProgress());
+            CultivationData.putInt("RealmStage", cultivatedPlayer.GetRealmStage().getLevel());
+
+            NbtCompound CultivationStagesNbt = new NbtCompound();
+            for (Map.Entry<Integer, CultivationRealm> entry : cultivatedPlayer.GetCultivationStages().entrySet()) {
+                NbtCompound realmNbt = new NbtCompound();
+                realmNbt.putInt("Id", entry.getValue().getId());
+                CultivationStagesNbt.put("Realm" + entry.getKey(), realmNbt);
+            }
+            CultivationData.put("CultivationStages", CultivationStagesNbt);
+            nbt.put(namespace, CultivationData);
+        }
     }
 
     @Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
