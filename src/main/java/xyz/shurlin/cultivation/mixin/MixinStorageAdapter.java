4f70501f79d29e6f72071fd2b390f7e9090e8a3b
EndsM <z2zz2zz@yahoo.com>
2024-05-04 20:26:33 +0800

    Update MixinStorageAdapter.java

--- src/main/java/xyz/shurlin/cultivation/mixin/MixinStorageAdapter.java
+++ src/main/java/xyz/shurlin/cultivation/mixin/MixinStorageAdapter.java
@@ -1,7 +1,12 @@
 package xyz.shurlin.cultivation.mixin;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.shurlin.cultivation.interfaces.StorageAdapter;
 import xyz.shurlin.cultivation.models.CultivationRealm;
 import xyz.shurlin.cultivation.models.enums.CultivationType;
@@ -10,7 +15,18 @@ import java.util.Collections;
 import java.util.List;
 
 @Mixin(PlayerEntity.class)
-public class MixinStorageAdapter implements StorageAdapter {
+public abstract class MixinStorageAdapter implements StorageAdapter {
+    @Shadow
+    public abstract void resetLastAttackedTicks();
+
+    @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
+    private void writeToNbt(NbtCompound nbt, CallbackInfo ci) {
+    }
+
+    @Inject(method = "readCustomDataFromNbt", at=@At("TAIL"))
+    private void readFromNbt(NbtCompound nbt, CallbackInfo ci) {
+    }
+
     @Override
     public boolean SaveCultivationType() {
         // To be implemented
