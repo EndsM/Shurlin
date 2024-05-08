e1996217a859e417e1e8e41d81d2319eaa2c8750
EndsM <z2zz2zz@yahoo.com>
2024-05-08 18:30:46 +0800

    Create CultivationInfoScreen.java
    Go read document before working on generic UI stuff

---
+++ src/main/java/xyz/shurlin/cultivation/CultivationInfoScreen.java
@@ -0,0 +1,11 @@
+package xyz.shurlin.cultivation;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class CultivationInfoScreen extends Screen {
+
+    protected CultivationInfoScreen(Text title) {
+        super(title);
+    }
+}
