a1f89a89e34577b9be14afe127151d88d3b929de
EndsM <z2zz2zz@yahoo.com>
2024-05-08 18:50:04 +0800

    Add some comment to it

--- src/main/java/xyz/shurlin/cultivation/CultivationInfoScreen.java
+++ src/main/java/xyz/shurlin/cultivation/CultivationInfoScreen.java
@@ -4,7 +4,8 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
 public class CultivationInfoScreen extends Screen {
-
+    // About HandledScreen and ScreenHandler, they would look like razor page in ASP.NET.
+    // HandledScreen is the page, and ScreenHandler is the PageModel ish thing
     protected CultivationInfoScreen(Text title) {
         super(title);
     }
