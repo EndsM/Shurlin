0b2a731e18df964c97019caa61e695d7f70ebb29
EndsM <z2zz2zz@yahoo.com>
2024-05-09 19:01:06 +0800

    Format codes
    fix them later

--- src/main/java/xyz/shurlin/cultivation/gui/CultivationInfoScreenHandler.java
+++ src/main/java/xyz/shurlin/cultivation/gui/CultivationInfoScreenHandler.java
@@ -2,12 +2,10 @@ package xyz.shurlin.cultivation.gui;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.screen.ScreenHandlerType;
-import org.jetbrains.annotations.Nullable;
 import xyz.shurlin.registry.client.ModScreenHandlerTypes;
 
 public class CultivationInfoScreenHandler extends ScreenHandler {
-    public CultivationInfoScreenHandler( int syncId) {
+    public CultivationInfoScreenHandler(int syncId) {
         super(ModScreenHandlerTypes.CULTIVATION_INFO, syncId);
     }
 
--- src/main/java/xyz/shurlin/registry/client/ModScreenHandlerTypes.java
+++ src/main/java/xyz/shurlin/registry/client/ModScreenHandlerTypes.java
@@ -1,9 +1,6 @@
 package xyz.shurlin.registry.client;
 
-import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
-import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.fabricmc.fabric.impl.screenhandler.ExtendedScreenHandlerType;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -14,6 +11,6 @@ public class ModScreenHandlerTypes {
     public static ScreenHandlerType<CultivationInfoScreenHandler> CULTIVATION_INFO = new ExtendedScreenHandlerType<>((syncId, inventory, buf) -> new CultivationInfoScreenHandler(syncId));
 
     public static void Register() {
-        Registry.register(Registry.SCREEN_HANDLER,new Identifier(Shurlin.MODID,"cultivation_info"),CULTIVATION_INFO);
+        Registry.register(Registry.SCREEN_HANDLER, new Identifier(Shurlin.MODID, "cultivation_info"), CULTIVATION_INFO);
     }
 }
