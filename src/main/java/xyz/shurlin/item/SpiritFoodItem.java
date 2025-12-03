9527d2456eab069f9e1a068f95a31efd24a19448
EndsM <lonhongsy@gmail.com>
2025-12-03 23:45:40 +0800

    Start work on the finishUsing effect

--- src/main/java/xyz/shurlin/item/SpiritFoodItem.java
+++ src/main/java/xyz/shurlin/item/SpiritFoodItem.java
@@ -1,7 +1,10 @@
 package xyz.shurlin.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class SpiritFoodItem extends Item {
     private final double spiritValue;
@@ -10,5 +13,10 @@ public class SpiritFoodItem extends Item {
         super(new Item.Settings().group(ItemGroups.SHURLIN)
                 .food(new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).alwaysEdible().build()));
         this.spiritValue = spiritValue;
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        return super.finishUsing(stack, world, user);
     }
 }
