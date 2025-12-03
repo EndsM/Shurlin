77bea46746f9ce40e0bf39bdf6ce892fa87deca8
EndsM <lonhongsy@gmail.com>
2025-12-03 23:49:14 +0800

    Implement the basic logic for eat to grow

--- src/main/java/xyz/shurlin/item/SpiritFoodItem.java
+++ src/main/java/xyz/shurlin/item/SpiritFoodItem.java
@@ -1,10 +1,13 @@
 package xyz.shurlin.item;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
 import net.minecraft.world.World;
+import xyz.shurlin.cultivation.interfaces.StorageAdapter;
 
 public class SpiritFoodItem extends Item {
     private final double spiritValue;
@@ -17,6 +20,22 @@ public class SpiritFoodItem extends Item {
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        return super.finishUsing(stack, world, user);
+        ItemStack result = super.finishUsing(stack, world, user);
+
+        // Server-side logic
+        if (!world.isClient && user instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) user;
+
+            if (player instanceof StorageAdapter) {
+                StorageAdapter cultivation = (StorageAdapter) player;
+
+                // Add the progress
+                cultivation.addCultivationProgress(this.spiritValue);
+
+                // Basic Feedback
+                player.sendMessage(new LiteralText("§aYou absorbed " + this.spiritValue + " spirit energy."), true);
+            }
+        }
+        return result;
     }
 }
