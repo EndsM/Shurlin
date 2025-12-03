6e6ae516feb39ba9ddae88827ad9cbe04849e635
EndsM <lonhongsy@gmail.com>
2025-12-03 23:38:09 +0800

    Create a SpiritFoodItem for future usage

--- src/main/java/xyz/shurlin/item/SpiritFoodItem.java
+++ src/main/java/xyz/shurlin/item/SpiritFoodItem.java
@@ -1,4 +1,14 @@
 package xyz.shurlin.item;
 
-public class SpiritFoodItem {
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+
+public class SpiritFoodItem extends Item {
+    private final double spiritValue;
+
+    public SpiritFoodItem(double spiritValue) {
+        super(new Item.Settings().group(ItemGroups.SHURLIN)
+                .food(new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).alwaysEdible().build()));
+        this.spiritValue = spiritValue;
+    }
 }
