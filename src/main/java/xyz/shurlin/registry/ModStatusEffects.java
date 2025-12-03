ed41e8a6b47bf122321fd2f8d1021612b803582e
EndsM <lonhongsy@gmail.com>
2025-12-04 01:22:56 +0800

    Create ModStatusEffects.java

---
+++ src/main/java/xyz/shurlin/registry/ModStatusEffects.java
@@ -0,0 +1,9 @@
+package xyz.shurlin.registry;
+
+import net.minecraft.entity.effect.StatusEffect;
+
+public class ModStatusEffects {
+    private static void registerStatusEffect (String id, StatusEffect statusEffect){
+
+    }
+}
