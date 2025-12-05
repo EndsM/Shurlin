868b9c2bb34ca1e542e0debc6dc4ea4e15866cab
EndsM <lonhongsy@gmail.com>
2025-12-05 18:49:42 +0800

    Register the shurlin path

--- src/main/java/xyz/shurlin/registry/ModCultivations.java
+++ src/main/java/xyz/shurlin/registry/ModCultivations.java
@@ -1,12 +1,20 @@
 package xyz.shurlin.registry;
 
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import xyz.shurlin.Shurlin;
+import xyz.shurlin.cultivation.CultivationRegistry;
 import xyz.shurlin.cultivation.models.CultivationRealm;
 import xyz.shurlin.cultivation.models.CultivationType;
 
 public class ModCultivations {
     public static final CultivationType SHURLIN_PATH = new CultivationType.Builder()
-            .addRealm(new CultivationRealm("realm.shurlin.soldier", 9, 15.0, 1.2))
-            .addRealm(new CultivationRealm("realm.shurlin.emissary", 9, 50.0, 1.2))
-            .addRealm(new CultivationRealm("realm.shurlin.teacher", 3, 200.0, 1.3))
+            .addRealm(new CultivationRealm("realm.shurlin.soldier", 9, 20.0, 1.4))
+            .addRealm(new CultivationRealm("realm.shurlin.emissary", 9, 350.0, 1.3))
+            .addRealm(new CultivationRealm("realm.shurlin.teacher", 9, 3000.0, 1.25))
             .build();
+
+    public static void Register() {
+        Registry.register(CultivationRegistry.INSTANCE, new Identifier(Shurlin.MODID, "shurlin_path"), SHURLIN_PATH);
+    }
 }
