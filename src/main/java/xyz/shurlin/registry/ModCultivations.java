bd92212d6d9e3749ca9a341063b5a322fe5ef3df
EndsM <lonhongsy@gmail.com>
2025-12-05 18:45:21 +0800

    Start adding the Shurlin Path registry

--- src/main/java/xyz/shurlin/registry/ModCultivations.java
+++ src/main/java/xyz/shurlin/registry/ModCultivations.java
@@ -1,4 +1,12 @@
 package xyz.shurlin.registry;
 
+import xyz.shurlin.cultivation.models.CultivationRealm;
+import xyz.shurlin.cultivation.models.CultivationType;
+
 public class ModCultivations {
+    public static final CultivationType SHURLIN_PATH = new CultivationType.Builder()
+            .addRealm(new CultivationRealm("realm.shurlin.soldier", 9, 15.0, 1.2))
+            .addRealm(new CultivationRealm("realm.shurlin.emissary", 9, 50.0, 1.2))
+            .addRealm(new CultivationRealm("realm.shurlin.teacher", 3, 200.0, 1.3))
+            .build();
 }
