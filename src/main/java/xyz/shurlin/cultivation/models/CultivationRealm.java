604d607d2c2b305cb9d1048eb00354fd4e4f8f5b
EndsM <lonhongsy@gmail.com>
2025-12-05 17:10:03 +0800

    Replace a simple progress with a map of requirements

--- src/main/java/xyz/shurlin/cultivation/models/CultivationRealm.java
+++ src/main/java/xyz/shurlin/cultivation/models/CultivationRealm.java
@@ -1,5 +1,10 @@
 package xyz.shurlin.cultivation.models;
 
+import xyz.shurlin.cultivation.models.enums.RealmStage;
+
+import java.util.HashMap;
+import java.util.Map;
+
 // This class represent one stage in stages of a cultivation system
 public class CultivationRealm {
     private int id;
@@ -8,8 +13,8 @@ public class CultivationRealm {
     // Sometimes the name of the energy from the cultivation system would change according to realm
     // example: 真气->真元
     private String energyNameKey;
-    // cultivation exp needed in this realm to the next stage
-    private double progressBar;
+    // Uses a map to pair the realm stages and value needed
+    private Map<RealmStage, Double> stageRequirements = new HashMap<>();
 
     public int getId() {
         return id;
@@ -25,5 +30,13 @@ public class CultivationRealm {
 
     public void setNameKey(String nameKey) {
         this.nameKey = nameKey;
+    }
+
+    public void setStageRequirement(RealmStage stage, double requirement) {
+        this.stageRequirements.put(stage, requirement);
+    }
+
+    public double getStageRequirement(RealmStage stage) {
+        return this.stageRequirements.getOrDefault(stage, 100.0);
     }
 }
