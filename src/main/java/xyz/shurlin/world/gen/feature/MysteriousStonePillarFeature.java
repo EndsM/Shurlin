052e48139499ef078dc0631037c30062772f4db0
EndsM <lonhongsy@gmail.com>
2025-12-06 23:19:04 +0800

    Cultivation Crystal is now at top of pillars

--- src/main/java/xyz/shurlin/world/gen/feature/MysteriousStonePillarFeature.java
+++ src/main/java/xyz/shurlin/world/gen/feature/MysteriousStonePillarFeature.java
@@ -21,14 +21,25 @@ public class MysteriousStonePillarFeature extends Feature<DefaultFeatureConfig> 
     public boolean generate(StructureWorldAccess serverWorldAccess, ChunkGenerator chunkGenerator, Random random, BlockPos pos, DefaultFeatureConfig config) {
         pos = pos.north(random.nextInt(16)).east(random.nextInt(16));
         pos = serverWorldAccess.getTopPosition(Heightmap.Type.WORLD_SURFACE_WG, pos);
-        BlockState state = ModBlocks.MYSTERIOUS_STONE.getDefaultState();
-        serverWorldAccess.setBlockState(pos.north(), state, 3);
-        serverWorldAccess.setBlockState(pos.south(), state, 3);
-        serverWorldAccess.setBlockState(pos.west(), state, 3);
-        serverWorldAccess.setBlockState(pos.east(), state, 3);
-        for (int i = 0; i < 8; i++) {
-            serverWorldAccess.setBlockState(pos.up(i), state, 3);
+
+
+        BlockState stoneState = ModBlocks.MYSTERIOUS_STONE.getDefaultState();
+        BlockState crystalState = ModBlocks.CULTIVATION_CRYSTAL.getDefaultState();
+
+        serverWorldAccess.setBlockState(pos.north(), stoneState, 3);
+        serverWorldAccess.setBlockState(pos.south(), stoneState, 3);
+        serverWorldAccess.setBlockState(pos.west(), stoneState, 3);
+        serverWorldAccess.setBlockState(pos.east(), stoneState, 3);
+
+        // Add a variation space
+        int height = 6 + random.nextInt(4);
+
+        for (int i = 0; i < height; i++) {
+            serverWorldAccess.setBlockState(pos.up(i), stoneState, 3);
         }
+        // The place the crystal at the top
+        serverWorldAccess.setBlockState(pos.up(height), crystalState, 3);
+
         return true;
     }
 }
