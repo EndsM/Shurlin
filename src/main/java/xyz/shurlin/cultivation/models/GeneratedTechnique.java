6c7e2b898d7003b4e23c434e2bb3339b12e47bcb
EndsM <lonhongsy@gmail.com>
2025-12-07 22:04:19 +0800

    keep work on model for generated techniques

--- src/main/java/xyz/shurlin/cultivation/models/GeneratedTechnique.java
+++ src/main/java/xyz/shurlin/cultivation/models/GeneratedTechnique.java
@@ -1,5 +1,6 @@
 package xyz.shurlin.cultivation.models;
 
+import net.minecraft.util.Identifier;
 import xyz.shurlin.cultivation.dynamic.TechniqueGrade;
 import xyz.shurlin.cultivation.dynamic.TechniqueQuality;
 
@@ -10,5 +11,8 @@ public class GeneratedTechnique {
     private String name;
     private TechniqueGrade grade;
     private TechniqueQuality quality;
-    
+    private Identifier elementId;
+
+    // The stats of the Technique
+    private double efficiency;
 }
