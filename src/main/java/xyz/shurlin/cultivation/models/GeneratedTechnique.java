e022237544ff139fdeb00902e430fb3725a227f6
EndsM <lonhongsy@gmail.com>
2025-12-07 22:01:53 +0800

    Create GeneratedTechnique.java

---
+++ src/main/java/xyz/shurlin/cultivation/models/GeneratedTechnique.java
@@ -0,0 +1,14 @@
+package xyz.shurlin.cultivation.models;
+
+import xyz.shurlin.cultivation.dynamic.TechniqueGrade;
+import xyz.shurlin.cultivation.dynamic.TechniqueQuality;
+
+import java.util.UUID;
+
+public class GeneratedTechnique {
+    private UUID id;
+    private String name;
+    private TechniqueGrade grade;
+    private TechniqueQuality quality;
+    
+}
