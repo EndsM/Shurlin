36b03b2e35af2f881a79fa1e6a1587297cb12774
EndsM <lonhongsy@gmail.com>
2025-12-07 20:27:48 +0800

    Create ModElements.java

---
+++ src/main/java/xyz/shurlin/registry/ModElements.java
@@ -0,0 +1,22 @@
+package xyz.shurlin.registry;
+
+import net.minecraft.util.Identifier;
+import xyz.shurlin.Shurlin;
+import xyz.shurlin.cultivation.models.SpiritElement;
+
+public class ModElements {
+    // In the future, the originally named "type", will be replaced with this proper SpiritElement setting
+    public static final SpiritElement METAL = new SpiritElement(new Identifier(Shurlin.MODID, "metal"), 0xFFD700);
+    public static final SpiritElement WOOD = new SpiritElement(new Identifier(Shurlin.MODID, "wood"), 0x00FF00);
+    public static final SpiritElement WATER = new SpiritElement(new Identifier(Shurlin.MODID, "water"), 0x0000FF);
+    public static final SpiritElement FIRE = new SpiritElement(new Identifier(Shurlin.MODID, "fire"), 0xFF0000);
+    public static final SpiritElement EARTH = new SpiritElement(new Identifier(Shurlin.MODID, "earth"), 0x8B4513);
+    public static final SpiritElement WIND = new SpiritElement(new Identifier(Shurlin.MODID, "wind"), 0xADD8E6);
+    public static final SpiritElement LIGHT = new SpiritElement(new Identifier(Shurlin.MODID, "light"), 0xFFFFFF);
+    public static final SpiritElement DARKNESS = new SpiritElement(new Identifier(Shurlin.MODID, "darkness"), 0x000000);
+    public static final SpiritElement POISON = new SpiritElement(new Identifier(Shurlin.MODID, "poison"), 0x800080);
+    public static final SpiritElement LIGHTNING = new SpiritElement(new Identifier(Shurlin.MODID, "lightning"), 0xFFFF00);
+    public static final SpiritElement ICE = new SpiritElement(new Identifier(Shurlin.MODID, "ice"), 0xE0FFFF);
+    public static final SpiritElement TIME_SPACE = new SpiritElement(new Identifier(Shurlin.MODID, "time_space"), 0x808080);
+
+}
