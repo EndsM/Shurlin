0359cefcf1173da80f83ffbce4c3333a13d835e9
EndsM <lonhongsy@gmail.com>
2025-12-07 19:50:13 +0800

    Create CultivationTechnique.java

---
+++ src/main/java/xyz/shurlin/cultivation/models/CultivationTechnique.java
@@ -0,0 +1,11 @@
+package xyz.shurlin.cultivation.models;
+
+import net.minecraft.util.Identifier;
+
+public class CultivationTechnique {
+    private final Identifier id;
+
+    public CultivationTechnique(Identifier id) {
+        this.id = id;
+    }
+}
