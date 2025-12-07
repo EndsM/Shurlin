28a4d7a19d2f6ba05d29d426306fbe57ef37683c
EndsM <lonhongsy@gmail.com>
2025-12-07 20:19:03 +0800

    Finish adding the interface

--- src/main/java/xyz/shurlin/cultivation/interfaces/CultivationLogic.java
+++ src/main/java/xyz/shurlin/cultivation/interfaces/CultivationLogic.java
@@ -1,11 +1,17 @@
 package xyz.shurlin.cultivation.interfaces;
 
-import xyz.shurlin.cultivation.CultivationRegistry;
 import xyz.shurlin.cultivation.models.CultivatedPlayer;
 import xyz.shurlin.cultivation.models.CultivationRealm;
 import xyz.shurlin.cultivation.models.CultivationTechnique;
+import xyz.shurlin.cultivation.models.SpiritElement;
 
 public interface CultivationLogic {
     // Calculate the current max Qi of a player
-    double calculateMaxQi(CultivatedPlayer player, CultivationRealm cultivationRealm, CultivationTechnique technique);
+    double CalculateMaxQi(CultivatedPlayer player, CultivationRealm cultivationRealm, CultivationTechnique technique);
+
+    // Calculate the Qi regen of player per tick
+    double CalculateQiRegen(CultivatedPlayer player, CultivationRealm cultivationRealm, CultivationTechnique technique);
+
+    // Get list of spirit elements for display
+    SpiritElement[] GetDisplayElements();
 }
--- src/main/java/xyz/shurlin/cultivation/models/CultivationType.java
+++ src/main/java/xyz/shurlin/cultivation/models/CultivationType.java
@@ -1,14 +1,18 @@
 package xyz.shurlin.cultivation.models;
 
+import xyz.shurlin.cultivation.interfaces.CultivationLogic;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class CultivationType {
     private final List<CultivationRealm> majorRealms;
+    private final CultivationLogic logic;
 
-    public CultivationType(List<CultivationRealm> majorRealms) {
+    public CultivationType(List<CultivationRealm> majorRealms, CultivationLogic logic) {
         this.majorRealms = majorRealms;
+        this.logic = logic;
     }
 
     public CultivationRealm getRealm(int index) {
@@ -22,16 +26,29 @@ public class CultivationType {
         return majorRealms.size();
     }
 
+    public CultivationLogic getLogic() {
+        return logic;
+    }
+
     public static class Builder {
         private final List<CultivationRealm> realms = new ArrayList<>();
+        private CultivationLogic logic;
 
         public Builder addRealm(CultivationRealm realm) {
             this.realms.add(realm);
             return this;
         }
 
+        public Builder setLogic(CultivationLogic logic) {
+            this.logic = logic;
+            return this;
+        }
+
         public CultivationType build() {
-            return new CultivationType(realms);
+            if (logic == null) {
+                throw new IllegalStateException("There are not logic set for this Cultivation Type!");
+            }
+            return new CultivationType(realms, logic);
         }
     }
 }
